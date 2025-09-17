f6f8d1f86c3a7a13171138ca5e76da0d7b96e125
lakrounehamza <lakrounehamza10@gmail.com>
2025-09-17 09:52:44 +0100

    feat :   ajouter la classe de connexion à la base de données

--- src/Models/ConnectionBase.java
+++ src/Models/ConnectionBase.java
@@ -1,15 +1,39 @@
-package models;
-
-
+package Models;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
-public class DataBase {
-    private   static   DataBase   instance;
-    private Connection  connection;
-    public DataBase(){
-        connection = new
+public class ConnectionBase {
+    private static ConnectionBase instance;
+    private Connection connection;
+
+    // Paramètres de connexion
+    private final String url = "jdbc:mysql://localhost:3306/banque";
+    private final String username = "root";
+    private final String password = "lakroune";
+
+     private ConnectionBase() {
+        try {
+             Class.forName("com.mysql.cj.jdbc.Driver");
+             this.connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connexion réussie à MySQL !");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erreur : Driver JDBC introuvable !");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Erreur de connexion MySQL !");
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionBase getInstance() {
+        if (instance == null) {
+            instance = new ConnectionBase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
